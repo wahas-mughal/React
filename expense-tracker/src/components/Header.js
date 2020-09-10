b8176dda0388e60316ecf804dd631074bdc14b7b
wahas_mughal <wahasali22@gmail.com>
2020-09-10 19:12:42 +0500

    add expense tracker

---
+++ expense-tracker/src/components/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Header() {
+  return (
+    <>
+      <div className = "header">
+        <h1>Expense Tracker {/*//heading */}</h1>
+      </div>
+    </>
+  );
+}
